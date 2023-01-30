8e3c9ff0729b8a94b28593c07cc476354349ceaa
nick5616 <nickbelovoskey@gmail.com>
2023-01-30 01:17:05 -0800

    Styling

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./styles/introduction.css";
 export function Introduction(): JSX.Element {
-  return <div className="intruduction-container"></div>;
+  return (
+    <div className="introduction-container">
+      <h1>Welcome to my Art portfolio</h1>
+      <h2>I hope you enjoy my art. I sure do enjoy drawing it 👼🏼📲</h2>
+    </div>
+  );
 }
