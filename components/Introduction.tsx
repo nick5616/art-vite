cda7166c4cc550df48a2c0112cfd9c972edc5048
Nicolas Belovoskey <nickbelovoskey@gmail.com>
2023-01-30 01:18:16 -0800

    Add art (#3)
    * Add art
    
    * Styling

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./styles/introduction.css";
-export default function Introduction(): JSX.Element {
-  return <div className="intruduction-container"></div>;
+export function Introduction(): JSX.Element {
+  return (
+    <div className="introduction-container">
+      <h1>Welcome to my Art portfolio</h1>
+      <h2>I hope you enjoy my art. I sure do enjoy drawing it 👼🏼📲</h2>
+    </div>
+  );
 }
