3f2815229a7acd124b75b7db3a2eb80fc46222e6
nick5616 <nickbelovoskey@gmail.com>
2023-01-30 23:16:01 -0800

    remove default

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import "./styles/introduction.css";
-export default function Introduction(): JSX.Element {
+export function Introduction(): JSX.Element {
   return <div className="intruduction-container"></div>;
 }
