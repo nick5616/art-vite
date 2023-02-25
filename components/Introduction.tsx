4775456003e7752dd444ca41f993b8baedf3cfeb
Nicolas Belovoskey <nickbelovoskey@gmail.com>
2023-02-25 05:58:07 -0800

    introduction work and prettier config tabs 4 space (#6)

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./styles/introduction.css";
 export function Introduction(): JSX.Element {
-  return (
-    <div className="introduction-container">
-      <h1>Welcome to my Art portfolio</h1>
-      <h2>I hope you enjoy my art. I sure do enjoy drawing it 👼🏼📲</h2>
-    </div>
-  );
+    return (
+        <div className="page-view">
+            <div className="horizontal-stack">
+                <div className="vertical-stack">
+                    <div className="introduction-container">
+                        <h1 className="serif heading">
+                            Hi! I'm Nick, but you can call me iPad baby
+                        </h1>
+                        <h2 className="serif">
+                            I hope you enjoy my art. I sure do enjoy drawing it
+                            👼🏼📲
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
