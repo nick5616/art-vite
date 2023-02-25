a8f9fc0484fad5fd750cd011312081786008832e
nick5616 <nickbelovoskey@gmail.com>
2023-02-25 15:37:29 -0800

    minimal styling

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -3,18 +3,13 @@ import "./styles/introduction.css";
 export function Introduction(): JSX.Element {
     return (
         <div className="page-view">
-            <div className="horizontal-stack">
-                <div className="vertical-stack">
-                    <div className="introduction-container">
-                        <h1 className="serif heading">
-                            Hi! I'm Nick, but you can call me iPad baby
-                        </h1>
-                        <h2 className="serif">
-                            I hope you enjoy my art. I sure do enjoy drawing it
-                            👼🏼📲
-                        </h2>
-                    </div>
-                </div>
+            <div className="introduction-container">
+                <h1 className="serif heading">
+                    Hi! I'm Nick, but you can call me iPad baby
+                </h1>
+                <h2 className="serif">
+                    I hope you enjoy my art. I sure do enjoy drawing it 👼🏼📲
+                </h2>
             </div>
         </div>
     );
