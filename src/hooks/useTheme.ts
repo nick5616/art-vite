7223392b9b854b5a8b86b1ae7f401ede8dde19dc
nick5616 <nickbelovoskey@gmail.com>
2023-10-29 13:33:57 -0700

    remove dead code

--- src/hooks/useTheme.ts
+++
@@ -1,5 +0,0 @@
-import { Theme } from "../models";
-import { getThemeFromVibe, ThemeAction } from "../theme";
-export function reducer(state: Theme, action: ThemeAction) {
-    return getThemeFromVibe(action.newVibe);
-}
